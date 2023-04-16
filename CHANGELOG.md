dc6d2f21fca38d5e2dca08157cbe310c43ba42f7
Alw3ys <alw3ys@gmail.com>
2023-04-16 14:30:54 +0200

    release version 0.4.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.4.3] - 2023-04-16
+
+### Added
+- Add -h, --help arguments
+- Update help print for better readability
+- Add --version option
+
 ## [0.4.2] - 2023-04-09
 
 ### Added
--- bin.js
+++ bin.js
@@ -2,21 +2,30 @@
 const path = require("path");
 const { buildDocs, pushDocs } = require("./");
 
-const help = () => {
-  console.log(`Usage:
-  enhancedocs build <docs_path>
-  enhancedocs push <project_id>
-  enhancedocs -v`);
+const help = (code) => {
+  console.log(`Usage: enhancedocs [options] [arguments]
+
+Options:
+
+  build [...contentPath]  Bundle ingest content into one file
+  push [projectId]        Push bundled content file to EnhanceDocs API
+  -v, --version           Print EnhanceDocs CLI version
+  -h, --help              Print EnhanceDocs command line options (currently set)`);
   process.exit(code);
 }
 
+const version = () => console.log(require(path.join(__dirname, './package.json')).version);
+
 const [, , cmd, ...args] = process.argv;
 const ln = args.length;
-const [x, y] = args;
+const [x, y, z] = args;
 const cmds = {
   build: () => buildDocs(x),
   push: () => pushDocs(x),
-  ['-v']: () => console.log(require(path.join(__dirname, './package.json')).version),
+  ['-v']: () => version(),
+  ['--version']: () => version(),
+  ['-h']: () => help(0),
+  ['--help']: () => help(0),
 };
 try {
   cmds[cmd] ? cmds[cmd]() : help(0);
