96736f22cce476e7b5a287e1a249eb7f3034e3c5
Alw3ys <alw3ys@gmail.com>
2023-04-27 21:25:20 +0200

    0.5.2

--- src/enhancedocs/config.py
+++ src/enhancedocs/config.py
@@ -1,11 +1,14 @@
 import os
 
+ENVIRONMENT_VARIABLES_HELP = """
+Environment variables:
+API_BASE_URL                    Set your Server API URL. (default: https://api.enhancedocs.com)
+ENHANCEDOCS_API_KEY             Set your API Key.
+ENHANCEDOCS_TELEMETRY_DISABLED  Set any value to disable telemetry.
+Documentation can be found at https://docs.enhancedocs.com/
+"""
+
 telemetry_disabled = os.environ.get("ENHANCEDOCS_TELEMETRY_DISABLED")
-
-
-def telemetry() -> bool:
-    return not telemetry_disabled
-
 file_path = ".enhancedocs/output.jsonp"
 api_base_url = os.environ.get("API_BASE_URL")
 if api_base_url is None:
@@ -15,6 +18,10 @@ api_key = os.environ.get("ENHANCEDOCS_API_KEY")
 headers = {}
 if api_key is not None:
     headers['authorization'] = f'Bearer {api_key}'
+
+
+def telemetry() -> bool:
+    return not telemetry_disabled
 
 
 if telemetry():
--- src/enhancedocs/main.py
+++ src/enhancedocs/main.py
@@ -1,11 +1,18 @@
 import click
 
+from .config import ENVIRONMENT_VARIABLES_HELP
 from .commands.ask import ask
 from .commands.build import build
 from .commands.push import push
 
 
-@click.group()
+class CustomHelpGroup(click.Group):
+    def get_help(self, ctx):
+        default_help = super().get_help(ctx)
+        return f"{default_help}\n{ENVIRONMENT_VARIABLES_HELP}"
+
+
+@click.group(cls=CustomHelpGroup)
 @click.version_option(None, *("-v", "--version"), package_name="enhancedocs")
 @click.help_option(*("-h", "--help"))
 def cli():
